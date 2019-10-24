12ccabbc742642e885f0ceb8785ed099bd9cba1d
jesus jorge <castrojesusjorge@gmail.com>
2019-10-24 20:38:39 -0300

    maquetado principal

--- vendor/php/includes/navbar.php
+++ vendor/php/includes/navbar.php
@@ -1,6 +1,6 @@
 <nav class="navbar navbar-expand navbar-dark bg-dark static-top">
 
-    <a class="navbar-brand mr-1" href="index.html">Sistema Red TG 2.0</a>
+    <a class="navbar-brand mr-1" href="index.html"><img src="imagenes/logointi2.svg" alt="logointi"></a>
 
     <button class="btn btn-link btn-sm text-white order-1 order-sm-0" id="sidebarToggle" href="#">
       <i class="fas fa-bars"></i>
@@ -9,7 +9,7 @@
     <!-- Navbar Search -->
     <form class="d-none d-md-inline-block form-inline ml-auto mr-0 mr-md-3 my-2 my-md-0">
       <div class="input-group">
-        <input type="text" class="form-control" placeholder="Search for..." aria-label="Search" aria-describedby="basic-addon2">
+        <input type="text" class="form-control" placeholder="Buscar por..." aria-label="Search" aria-describedby="basic-addon2">
         <div class="input-group-append">
           <button class="btn btn-primary" type="button">
             <i class="fas fa-search"></i>
