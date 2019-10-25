207ca20a099281a34d0eedfcfe95a829f449476d
jesus jorge <castrojesusjorge@gmail.com>
2019-10-24 23:59:49 -0300

    modificado link navbar

--- vendor/php/includes/navbar.php
+++ vendor/php/includes/navbar.php
@@ -1,6 +1,6 @@
 <nav class="navbar navbar-expand navbar-dark bg-dark static-top">
 
-    <a class="navbar-brand mr-1" href="index.html"><img src="imagenes/logointi2.svg" alt="logointi"></a>
+    <a class="navbar-brand mr-1" href="index.php"><img src="imagenes/logointi2.svg" alt="logointi"></a>
 
     <button class="btn btn-link btn-sm text-white order-1 order-sm-0" id="sidebarToggle" href="#">
       <i class="fas fa-bars"></i>
