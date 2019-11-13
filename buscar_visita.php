dc688118744452bbc8ed6a66335d4d07d5e338e7
ignmand <ignaciofmandagaran@gmail.com>
2019-11-12 21:38:05 -0300

    se borran archivos y se resuelve logueo visitas

--- buscar_visita.php
+++ buscar_visita.php
@@ -1,7 +1,17 @@
-<?php 
+<?php
 session_start();
 include 'vendor/php/querys.php';
-include_once 'vendor/php/conexion.php';
+include 'vendor/php/conexion.php';
+
+//Comprobamos si el usario está logueado
+//Si no lo está, se le redirecciona al index
+//Si lo está, definimos el botón de cerrar sesión y la duración de la sesión
+if(!isset($_SESSION['usuario']) and $_SESSION['estado'] != 'Autenticado') {
+	header('Location: index.php');
+} else {
+	$estado = $_SESSION['usuario'];
+	require('vendor/php/sesiones.php');
+};
 ?>
 
 <!DOCTYPE html>
